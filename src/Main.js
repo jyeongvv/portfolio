d46aa4e1c6ff424a4baa6a6ece055c3da4ae0003
jyeongvv <kjy812467@gmail.com>
2023-10-21 18:48:54 +0900

    파일 정리

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navigation";
+import "../public/css/Main.css"
+
+function Main() {
+  return (
+    <div className="Main">
+      <Navbar /> 
+    </div>
+  );
+}
+
+export default Main;
