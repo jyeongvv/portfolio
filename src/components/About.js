22f1f8a3f8e22572ba8e7598561a09093355e6f5
jyeongvv <kjy812467@gmail.com>
2023-10-22 15:19:02 +0900

    about-header

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
 import React from "react";
 import '../css/About.css'
+import CircleIcon from '@mui/icons-material/Circle';
+
+const grey = 'grey';
 
 const About = () => {
     return (
         <div>
-            
+            <div className="about-head">
+                <p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <CircleIcon sx={{ color: grey, fontSize: 8, marginRight: 1 }} /> ABOUT ME <CircleIcon sx={{ color: grey, fontSize: 8, marginLeft: 1 }} />
+                </p>
+            </div>
         </div>
     )
 }
