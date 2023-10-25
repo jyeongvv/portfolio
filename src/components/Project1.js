39eecffa2658a993eb53ebb595927fe72cc4128b
jyeongvv <kjy812467@gmail.com>
2023-10-25 18:29:32 +0900

    image slider완..성

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -2,41 +2,35 @@ import React, { useState } from 'react';
 import '../css/Project1.css'
 
 function Project1() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const previousImage = () => {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
+
+  const nextImage = () => {
+    if (currentImageIndex < images.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  };
+
   const images = [
     'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
     'https://dl.dropbox.com/s/f8tnpnzfd2kn16e/image2.jpg?raw=1',
     'https://dl.dropbox.com/s/rjcb4wo9192rdnj/image3.jpg?raw=1',
     'https://dl.dropbox.com/s/oxcpep8398iwp7s/image4.jpg?raw=1',
+    'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
   ];
 
-  const next = () => {
-    setActiveIndex((activeIndex + 1) % images.length);
-  };
-
-  const prev = () => {
-    setActiveIndex((activeIndex - 1 + images.length) % images.length);
-  };
-
-  const dot = (index) => {
-    setActiveIndex(index);
-  };
-
   return (
-    <div className="project1-wrapper">
-        <div className="image1-container">
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              className={index === activeIndex ? 'active' : ''}
-              alt={`Image ${index + 1}`}
-            />
-          ))}
-        </div>
-
-        <button id="prev" onClick={prev}>&lt;</button>
-        <button id="next" onClick={next}>&gt;</button>
+    <div className="project1">
+      <div className="image-container">
+        <button className="project1-button" onClick={previousImage}>이전</button>
+        <img src={images[currentImageIndex]} alt="이미지" className="project1-image" />
+        <button className="project1-button" onClick={nextImage}>다음</button>
+      </div>
     </div>
   );
 }
--- src/components/Project2.js
+++ src/components/Project2.js
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import '../css/Project2.css'
+// import React, { useState } from 'react';
+// import '../css/Project2.css'
 
-function Project2() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const images = [
-    'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
-    'https://dl.dropbox.com/s/f8tnpnzfd2kn16e/image2.jpg?raw=1',
-    'https://dl.dropbox.com/s/rjcb4wo9192rdnj/image3.jpg?raw=1',
-    'https://dl.dropbox.com/s/oxcpep8398iwp7s/image4.jpg?raw=1',
-  ];
+// function Project2() {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const images = [
+//     'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
+//     'https://dl.dropbox.com/s/f8tnpnzfd2kn16e/image2.jpg?raw=1',
+//     'https://dl.dropbox.com/s/rjcb4wo9192rdnj/image3.jpg?raw=1',
+//     'https://dl.dropbox.com/s/oxcpep8398iwp7s/image4.jpg?raw=1',
+//   ];
 
-  const next = () => {
-    setActiveIndex((activeIndex + 1) % images.length);
-  };
+//   const next = () => {
+//     setActiveIndex((activeIndex + 1) % images.length);
+//   };
 
-  const prev = () => {
-    setActiveIndex((activeIndex - 1 + images.length) % images.length);
-  };
+//   const prev = () => {
+//     setActiveIndex((activeIndex - 1 + images.length) % images.length);
+//   };
 
-  const dot = (index) => {
-    setActiveIndex(index);
-  };
+//   const dot = (index) => {
+//     setActiveIndex(index);
+//   };
 
-  return (
-    <div className="project2-wrapper">
-        <div className="image2-container">
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              className={index === activeIndex ? 'active' : ''}
-              alt={`Image ${index + 1}`}
-            />
-          ))}
-        </div>
+//   return (
+//     <div className="project2-wrapper">
+//         <div className="image2-container">
+//           {images.map((image, index) => (
+//             <img
+//               key={index}
+//               src={image}
+//               className={index === activeIndex ? 'active' : ''}
+//               alt={`Image ${index + 1}`}
+//             />
+//           ))}
+//         </div>
 
-        <button id="prev" onClick={prev}>&lt;</button>
-        <button id="next" onClick={next}>&gt;</button>
-    </div>
-  );
-}
+//         <button id="prev" onClick={prev}>&lt;</button>
+//         <button id="next" onClick={next}>&gt;</button>
+//     </div>
+//   );
+// }
 
-export default Project2;
+// export default Project2;
