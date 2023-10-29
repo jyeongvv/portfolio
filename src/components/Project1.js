4d210267a0a104bf5a8362a7b9dc76f1de7f6737
jyeongvv <kjy812467@gmail.com>
2023-10-29 20:41:05 +0900

    슬라이드 방식 변경

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,26 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Project1.css';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import CircleIcon from '@mui/icons-material/Circle';
 
-const orange = '#FE763B';
+// const orange = '#FE763B';
 const grey = 'grey';
 
 function Project1() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [autoplay, setAutoplay] = useState(true);
 
-  const previousImage = () => {
-    if (currentImageIndex > 0) {
-      setCurrentImageIndex(currentImageIndex - 1);
-    }
-  };
-
-  const nextImage = () => {
-    if (currentImageIndex < images.length - 1) {
-      setCurrentImageIndex(currentImageIndex + 1);
-    }
-  };
 
   const images = [
     'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
@@ -30,30 +18,46 @@ function Project1() {
     'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
   ];
 
+  useEffect(() => {
+    let interval;
+
+    if (autoplay) {
+      interval = setInterval(() => {
+        if (currentImageIndex < images.length - 1) {
+          setCurrentImageIndex(currentImageIndex + 1);
+        } else {
+          setCurrentImageIndex(0);
+        }
+      }, 1500); // 1.5초
+    }
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [currentImageIndex, autoplay, images.length]);
+
+  const handleMouseEnter = () => {
+    setAutoplay(false);
+  };
+
+  const handleMouseLeave = () => {
+    setAutoplay(true);
+  };
+
   return (
-    <div className="project1">
+    <div className="project1" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="project-head">
-                <p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <CircleIcon sx={{ color: grey, fontSize: 8, marginRight: 1 }} /> PROJECT <CircleIcon sx={{ color: grey, fontSize: 8, marginLeft: 1 }} />
-                </p>
+        <p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <CircleIcon sx={{ color: grey, fontSize: 8, marginRight: 1 }} /> PROJECT <CircleIcon sx={{ color: grey, fontSize: 8, marginLeft: 1 }} />
+        </p>
       </div>
       <div className='project1-name'>
-              <p>
-                chalcock (찰칵)
-              </p>
+        <p>
+          chalcock (찰칵)
+        </p>
       </div>
       <div className="image-container">
-        <NavigateBeforeIcon
-          sx={{ color: orange, fontSize: 40, marginRight: 1 }}
-          onClick={previousImage}
-          className="project1-button"
-        />
         <img src={images[currentImageIndex]} alt="이미지" className="project1-image" />
-        <NavigateNextIcon
-          sx={{ color: orange, fontSize: 40, marginLeft: 1 }}
-          onClick={nextImage}
-          className="project1-button"
-        />
       </div>
     </div>
   );
--- src/components/Project2.js
+++ src/components/Project2.js
@@ -1,24 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Project2.css';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
-const orange = '#FE763B';
 
 function Project2() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [autoplay, setAutoplay] = useState(true);
 
-  const previousImage = () => {
-    if (currentImageIndex > 0) {
-      setCurrentImageIndex(currentImageIndex - 1);
-    }
-  };
-
-  const nextImage = () => {
-    if (currentImageIndex < images.length - 1) {
-      setCurrentImageIndex(currentImageIndex + 1);
-    }
-  };
 
   const images = [
     'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
@@ -28,27 +15,42 @@ function Project2() {
     'https://dl.dropbox.com/s/2315nuj3xyepfix/image1.jpg?raw=1',
   ];
 
+  useEffect(() => {
+    let interval;
+
+    if (autoplay) {
+      interval = setInterval(() => {
+        if (currentImageIndex < images.length - 1) {
+          setCurrentImageIndex(currentImageIndex + 1);
+        } else {
+          setCurrentImageIndex(0);
+        }
+      }, 1500); // 1.5초
+    }
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [currentImageIndex, autoplay, images.length]);
+
+  const handleMouseEnter = () => {
+    setAutoplay(false);
+  };
+
+  const handleMouseLeave = () => {
+    setAutoplay(true);
+  };
+
   return (
-    <div className="project2">
+    <div className="project2" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className='project2-name'>
-              <p>
-                my web portfolio
-              </p>
+        <p>
+          my web portfolio
+        </p>
       </div>
       <div className="image-container">
-        <NavigateBeforeIcon
-          sx={{ color: orange, fontSize: 40, marginRight: 1 }}
-          onClick={previousImage}
-          className="project2-button"
-        />
         <img src={images[currentImageIndex]} alt="이미지" className="project2-image" />
-        <NavigateNextIcon
-          sx={{ color: orange, fontSize: 40, marginLeft: 1 }}
-          onClick={nextImage}
-          className="project2-button"
-        />
       </div>
-      <div id="project-line"></div>
     </div>
   );
 }
